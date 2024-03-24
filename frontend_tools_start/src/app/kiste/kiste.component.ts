33b9381d7db16ca239f58d6d2c1f7176c7aaaa8b
elise <elise.schwentesius@student.htw-berlin.de>
2024-03-24 16:22:53 +0100

    erster backendservice get alles und filtern

--- frontend_tools_start/src/app/kiste/kiste.component.ts
+++ frontend_tools_start/src/app/kiste/kiste.component.ts
@@ -1,7 +1,9 @@
 import { NgFor } from '@angular/common';
-import { Component } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { Component, OnInit, inject } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { Tool } from '../shared/tool';
+import { BackendService } from '../shared/backend.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-kiste',
@@ -10,7 +12,42 @@ import { Tool } from '../shared/tool';
   templateUrl: './kiste.component.html',
   styleUrl: './kiste.component.css'
 })
-export class KisteComponent {
-  filterArray: Tool [] = [];
+export class KisteComponent implements OnInit {
 
+  ngOnInit(): void {
+    this.readAllTools();
+    this.filterArray = this.tools;
+    console.log('in table --> allMembers', this.tools)
+  }
+
+  bs = inject(BackendService);
+  tools: Tool[] = [];
+  filterArray: Tool [] = [];
+  search = new FormControl('');
+
+  readAllTools(): void {
+    this.bs.getAllTools().subscribe({
+      next: (response) => {
+        this.tools = response;
+        console.log(this.tools);
+        return this.tools;
+      },
+      error: (err) => console.log(err),
+      complete: () => console.log('getAllTools() completed')
+    })
+  }
+
+  filterMembers() {
+    console.log(this.search.value)
+    let searchString = this.search.value ? this.search.value : '';
+    console.log('in table --> searchString', searchString);
+
+    this.filterArray = this.tools.filter(   (tool)  => {
+      return (
+        tool.kategorie.toLowerCase().includes(searchString) ||
+        tool.artikel.toLowerCase().includes(searchString) ||
+        tool.details.toLowerCase().includes(searchString)
+      );
+    });
+  }
 }
--- frontend_tools_start/src/app/shared/backend.service.ts
+++ frontend_tools_start/src/app/shared/backend.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
+import { Tool } from './tool';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BackendService {
+  backendUrl = 'http://localhost:4000';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  // get all entries
+  getAllTools(): Observable<Tool[]> {
+    let endpoint = '/tools';
+    return this.http.get<Tool[]>(this.backendUrl + endpoint);
+  }
 }
