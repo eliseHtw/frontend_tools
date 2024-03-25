00d414c0dc86a582f30d00bdc23173a7d2b88bf4
elise <elise.schwentesius@student.htw-berlin.de>
2024-03-25 10:45:12 +0100

    kline änderungen

--- frontend_tools_start/src/app/shared/backend.service.ts
+++ frontend_tools_start/src/app/shared/backend.service.ts
@@ -17,9 +17,33 @@ export class BackendService {
     return this.http.get<Tool[]>(this.backendUrl + endpoint);
   }
 
+  // post one entry
+  postOneTool(tool: Tool): Observable<Tool> {
+    let endpoint = '/tools';
+    return this.http.post<Tool>(this.backendUrl + endpoint, tool);
+  }
+
+  // get one entry via id
+  getOneToolId(id: string): Observable<Tool>{
+    let endpoint = '/tools';
+    return this.http.get<Tool>(this.backendUrl + endpoint + '/' + id);
+  }
+
+  // get one entry via artiekl
+  getOneArtikel(artikel: string): Observable<Tool>{
+    let endpoint = '/tools';
+    return this.http.get<Tool>(this.backendUrl + endpoint + '/artikel/' + artikel);
+  }
+
   // delete one entry
   deleteOneTool(id:string): Observable<any> {
     let endpoint = '/tools';
-    return this.http.delete<any>(this.backendUrl + endpoint + "/" +id);
+    return this.http.delete<any>(this.backendUrl + endpoint + '/' + id);
+  }
+
+  // update entry
+  updateOneTool(tool: Tool, id: string): Observable<Tool> {
+    let endpoint = '/tools';
+    return this.http.put<Tool>(this.backendUrl + endpoint + '/' + id, tool);
   }
 }
